1e37befa0602d0039fd2932bf0290af511006e5c
Jonathan van der Meulen <32264711+JonaVDM@users.noreply.github.com>
2024-04-11 22:03:51 +0200

    feat: add nvfetcher
    Adds nvfetcher to automatically-ish update the dotfiles and neovim
    config from github.

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -1,41 +1,29 @@
 {  pkgs, ... }:
 
 let
- dotfiles = pkgs.fetchFromGitHub {
-   owner = "jonavdm";
-   repo = "dotfiles";
-   rev = "2b9da42ed0750457284cb1f9fae57e9bfc910519";
-   sha256 = "01ma81bb0lvqi43irha14sgm75miwyqz9gbmqrr8m1slmwvwx6b1";
-  };
-
-  nvimconfig = pkgs.fetchFromGitHub {
-    owner = "jonavdm";
-    repo = "neovim-config";
-    rev = "09911bb0030ab50414fdc0bab23c29d3af6734ab";
-    sha256 = "0hyyxspcvcv6g2f1mg6jm7l1rpq1fmy6700mg8w3pz1m34bcm09b";
-  };
+  gen = pkgs.callPackage ../../_sources/generated.nix { }; 
 in
 {
   home.file = {
     ".config/nvim" = {
-      source = "${nvimconfig}";
+      source = "${gen.nvim.src}";
       recursive = true;
     };
 
     ".p10k.zsh" = {
-      source = "${dotfiles}/.p10k.zsh";
+      source = "${gen.dotfiles.src}/.p10k.zsh";
     };
 
     ".gitmessage" = {
-      source = "${dotfiles}/.gitmessage";
+      source = "${gen.dotfiles.src}/.gitmessage";
     };
 
     ".config/alacritty/alacritty.toml" = {
-      source = "${dotfiles}/.alacritty.toml";
+      source = "${gen.dotfiles.src}/.alacritty.toml";
     };
 
     ".background-image" = {
-      source = "${dotfiles}/wallpapers/01.jpg";
+      source = "${gen.dotfiles.src}/wallpapers/01.jpg";
     };
   };
 }
