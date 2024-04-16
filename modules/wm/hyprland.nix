b90f27fe4037d4a8f9c7fd38091fb5439a165764
Jonathan van der Meulen <32264711+JonaVDM@users.noreply.github.com>
2024-04-16 19:15:44 +0200

    feat: add non-working electron fix for wayland

--- modules/wm/hyprland.nix
+++ modules/wm/hyprland.nix
@@ -6,4 +6,10 @@ lib.mkIf(config.wm.hyprland)
     enable = true;
     xwayland.enable = true;
   };
+
+  # This should be a fix for running electron apps in wayland, which always
+  # kinda seemed to work on my arch install. Weirdly enough it did not work
+  # here, like at all.
+  # Todo: make this stuff work
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
