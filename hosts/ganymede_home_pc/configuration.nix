ff26606d08f8351cd786caeb945be497a4be0447
Jonathan van der Meulen <32264711+JonaVDM@users.noreply.github.com>
2024-04-18 16:54:15 +0200

    feat: enable wake on lan

--- hosts/ganymede_home_pc/configuration.nix
+++ hosts/ganymede_home_pc/configuration.nix
@@ -81,6 +81,8 @@
     "aarch64-linux"
   ];
 
+  networking.interfaces.enp5s0.wakeOnLan.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
