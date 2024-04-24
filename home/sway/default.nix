32778712a14f900311754a0f788c543fe37cda65
Jonathan van der Meulen <32264711+JonaVDM@users.noreply.github.com>
2024-04-24 09:38:27 +0200

    config(sway): update sway config
    set background color on lock
    make use of waybar

--- home/sway/default.nix
+++ home/sway/default.nix
@@ -52,8 +52,8 @@ lib.mkIf(config.wm.sway)
     input "type:keyboard" {
       repeat_delay 200
       repeat_rate 50
-      xkb_layout us,se
-      xkb_options caps:escape,grp:rctrl_rshift_toggle
+      xkb_layout us
+      xkb_options caps:escape
     }
 
     #
@@ -71,7 +71,7 @@ lib.mkIf(config.wm.sway)
     # Start your launcher
     bindsym $mod+d exec $menu
 
-    bindsym $mod+Alt+l exec swaylock
+    bindsym $mod+Alt+l exec swaylock -c 000000
 
     # Drag floating windows by holding down $mod and left mouse button.
     # Resize them with right mouse button + $mod.
@@ -155,6 +155,10 @@ lib.mkIf(config.wm.sway)
     # Move focus to the parent container
     bindsym $mod+a focus parent
 
+    # Sreenshots
+    bindsym $mod+Print exec grim - | wl-copy
+    bindsym $mod+Shift+Print exec grim -g "$(slurp)" - | wl-copy
+
     bindsym $mod+r mode "resize"
     mode "resize" {
         bindsym $left resize shrink width 10px
@@ -188,53 +192,9 @@ lib.mkIf(config.wm.sway)
     include @sysconfdir@/sway/config.d/*
   '';
 
-  home.file.".config/sway/bar.sh" = {
-    text = ''
-      # The abbreviated weekday (e.g., "Sat"), followed by the ISO-formatted date
-      # like 2018-10-06 and the time (e.g., 14:01). Check `man date` on how to format
-      # time and date.
-      date_formatted=$(date "+%a %F %H:%M")
-
-      # "upower --enumerate | grep 'BAT'" gets the battery name (e.g.,
-      # "/org/freedesktop/UPower/devices/battery_BAT0") from all power devices.
-      # "upower --show-info" prints battery information from which we get
-      # the state (such as "charging" or "fully-charged") and the battery's
-      # charge percentage. With awk, we cut away the column containing
-      # identifiers. i3 and sway convert the newline between battery state and
-      # the charge percentage automatically to a space, producing a result like
-      # "charging 59%" or "fully-charged 100%".
-      battery_info=$(upower --show-info $(upower --enumerate |\
-      grep 'BAT') |\
-      egrep "state|percentage" |\
-      awk '{print $2}')
-
-      # "amixer -M" gets the mapped volume for evaluating the percentage which
-      # is more natural to the human ear according to "man amixer".
-      # Column number 4 contains the current volume percentage in brackets, e.g.,
-      # "[36%]". Column number 6 is "[off]" or "[on]" depending on whether sound
-      # is muted or not.
-      # "tr -d []" removes brackets around the volume.
-      # Adapted from https://bbs.archlinux.org/viewtopic.php?id=89648
-      audio_volume=$(amixer -M get Master |\
-      awk '/Mono.+/ {print $6=="[off]" ?\
-      $4" 🔇": \
-      $4" 🔉"}' |\
-      tr -d [])
-
-      # Additional emojis and characters for the status bar:
-      # Electricity: ⚡ ↯ ⭍ 🔌
-      # Audio: 🔈 🔊 🎧 🎶 🎵 🎤
-      # Separators: \| ❘ ❙ ❚
-      # Misc: 🐧 💎 💻 💡 ⭐ 📁 ↑ ↓ ✉ ✅ ❎
-      echo $audio_volume $battery_info 🔋 $date_formatted
-    '';
-
-    executable = true;
-  };
-
-  home.file.".config/waybar/config".text = ''
-    [
-
-    ]
-  '';
+  # home.file.".config/waybar/config".text = ''
+  #   [
+  #
+  #   ]
+  # '';
 }
