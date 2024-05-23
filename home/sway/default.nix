819ccb6025f190b2190c43b9e1377510565d1834
Jonathan van der Meulen <32264711+JonaVDM@users.noreply.github.com>
2024-05-23 07:50:53 +0200

    sway: add scratchpad

--- home/sway/default.nix
+++ home/sway/default.nix
@@ -172,6 +172,13 @@ lib.mkIf config.wm.sway
         bindsym Escape mode "default"
     }
 
+    # Move the currently focused window to the scratchpad
+    bindsym $mod+Shift+minus move scratchpad
+
+    # Show the next scratchpad window or hide the focused scratchpad window.
+    # If there are multiple scratchpad windows, this command cycles through them.
+    bindsym $mod+minus scratchpad show
+
     #
     # Status Bar:
     #
