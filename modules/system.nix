937bccd4fde4bf9cae4701d8e84b48faeaf302f4
Jonathan van der Meulen <32264711+JonaVDM@users.noreply.github.com>
2024-05-27 09:00:59 +0200

    fixed date displaying as dutch

--- modules/system.nix
+++ modules/system.nix
@@ -5,17 +5,6 @@
   time.timeZone = "Europe/Amsterdam";
 
   i18n.defaultLocale = "en_GB.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "nl_NL.UTF-8";
-    LC_IDENTIFICATION = "nl_NL.UTF-8";
-    LC_MEASUREMENT = "nl_NL.UTF-8";
-    LC_MONETARY = "nl_NL.UTF-8";
-    LC_NAME = "nl_NL.UTF-8";
-    LC_NUMERIC = "nl_NL.UTF-8";
-    LC_PAPER = "nl_NL.UTF-8";
-    LC_TELEPHONE = "nl_NL.UTF-8";
-    LC_TIME = "nl_NL.UTF-8";
-  };
 
   hardware.pulseaudio.enable = false;
   sound.enable = true;
