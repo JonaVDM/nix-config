5ec105c5dd658aea1ca5dbf1872dd3f6dce35dfa
Jonathan van der Meulen <32264711+JonaVDM@users.noreply.github.com>
2024-06-14 11:31:18 +0200

    add vscode extensions

--- home/apps/vscode.nix
+++ home/apps/vscode.nix
@@ -10,5 +10,21 @@
       jnoortheen.nix-ide
       vscodevim.vim
     ];
+
+    userSettings = {
+      "editor.bracketPairColorization.enabled" = true;
+      "editor.guides.bracketPairs" = "active";
+      "editor.lineHeight" = 25;
+      "editor.rulers" = [80 120];
+      "explorer.confirmDelete" = false;
+      "explorer.confirmDragAndDrop" = false;
+      "extensions.ignoreRecommendations" = true;
+      "files.insertFinalNewline" = true;
+      "files.trimTrailingWhitespace" = true;
+      "keyboard.dispatch" = "keyCode";
+      "telemetry.telemetryLevel" = "off";
+      "update.mode" = "none";
+      "workbench.colorTheme" = "Nord";
+    };
   };
 }
